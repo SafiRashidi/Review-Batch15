426b217b9959f475562d20dbe317a5c6d235fc85
SafiRashidi <safirashidi55@gmail.com>
2023-03-16 20:29:43 -0500

    Review 12

---
+++ src/Com/Syntax/review12/ExceptionDemo1.java
@@ -0,0 +1,12 @@
+package Com.Syntax.review12;
+
+public class ExceptionDemo1 {
+    public static void main(String[] args) {
+
+        String name = "!Batch 15 is SMART";
+        System.out.println(name.charAt(-1));
+
+
+
+    }
+}
---
+++ src/Com/Syntax/review12/ExceptionDemo2.java
@@ -0,0 +1,30 @@
+package Com.Syntax.review12;
+
+public class ExceptionDemo2 {
+    public static void main(String[] args) {
+
+        String name = "!Batch 15 is SMART";
+        try {
+            System.out.println(name.charAt(-1));
+        } catch (StringIndexOutOfBoundsException e) {
+            /*
+            printStackTrace
+            provides us all the information to find and fix the issue
+            As a tester you will always use printStackTrace
+             */
+            e.printStackTrace();
+            /*
+            if we are interested in only the message we can use getMessage method
+             */
+            //System.out.println(e.getMessage());
+/*
+e will print the complete information about the exception class plus the message
+ */
+        } catch (NullPointerException npe) {
+            System.out.println(npe.getMessage());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("rest of the program");
+    }
+}
